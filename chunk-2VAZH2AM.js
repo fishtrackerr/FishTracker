ecbbd058c7ae3f49b0ec0a7de2850175d5770757
erwint1994 <erwin.torrenga@live.nl>
2026-08-02 14:25:37 +0200

    Auto-generated commit

--- chunk-2VAZH2AM.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./chunk-KLUAH5YN.js";import{Z as s}from"./chunk-YLCVSZIS.js";var a=class e{async getReleaseNotes(){try{let t=await r("assets/release-notes.json");return t.ok?await t.json():null}catch{return null}}static \u0275fac=function(o){return new(o||e)};static \u0275prov=s({token:e,factory:e.\u0275fac,providedIn:"root"})};export{a};
